42fa27ea4530e20dce1896f3ef24795dcb6a65db
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2022-05-15 15:48:23 -0600

    Added Shipments model

--- inventory_project/inventory_app/models.py
+++ inventory_project/inventory_app/models.py
@@ -1,8 +1,32 @@
+from pyexpat import model
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Shipment(models.Model):
+    first_name = models.CharField(max_length=32)
+
+    last_name = models.CharField(max_length=32)
+
+    company = models.CharField(max_length=32)
+
+    street_address = models.CharField(max_length=64)
+
+    city = models.CharField(max_length=32)
+
+    state_province_region = models.CharField(max_length=32)
+
+    postal_code = models.CharField(max_length=32)
+
+    country = models.CharField(max_length=32)
+
+    ship_code = models.CharField(max_length=20)
+
+    tracking_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{} | {}".format(self.id, self.ship_code)
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -20,3 +44,5 @@ class Item(models.Model):
 
     def __str__(self):
         return "{} | {}".format(self.id, self.name)
+
+
